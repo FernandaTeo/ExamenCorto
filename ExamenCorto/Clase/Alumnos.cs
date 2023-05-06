2861329ccdc1d492688a41904885e116d566239e
Fernanda <aracelyteo16@gmail.com>
2023-05-06 11:07:10 -0600

    Agregar archivos de proyecto.

---
+++ ExamenCorto/Clase/Alumnos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamenCorto.Interface;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ejercicio4
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            IPersona persona = new Nombre("Juan", 30);
+            persona.Saludar();
+            persona.CumplirAños();
+            Console.WriteLine("La edad de la persona es: " + persona.Edad);
+        }
+    }
+}
---
+++ ExamenCorto/Interface/IPersona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.Interface
+{
+    {
+    public interface IPersona
+    {
+        string Nombre { get; }
+        int Edad { get; }
+        void Saludar();
+        void CumplirAños();
+    }
+
+    public class Persona : IPersona
+    {
+        private string _nombre;
+        private int _edad;
+
+        public Persona(string nombre, int edad)
+        {
+            _nombre = nombre;
+            _edad = edad;
+        }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.Interface
+    {
+    {
+    public interface IPersona
+        {
+            string Nombre { get; }
+            int Edad { get; }
+            void Saludar();
+            void CumplirAños();
+        }
+
+        public class Persona : IPersona
+        {
+            private string _nombre;
+            private int _edad;
+
+            public Persona(string nombre, int edad)
+            {
+                _nombre = nombre;
+                _edad = edad;
+            }
+
